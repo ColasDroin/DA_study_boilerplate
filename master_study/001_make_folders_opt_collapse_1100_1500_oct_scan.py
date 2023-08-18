eb1e58c3223333e19192bd58521e15115d882de1
Colas Droin <colasdroin@gmail.com>
2023-08-18 18:22:57 +0200

    Fixed octupoles

--- master_study/001_make_folders_opt_collapse_1100_1500_oct_scan.py
+++ master_study/001_make_folders_opt_collapse_1100_1500_oct_scan.py
@@ -283,7 +283,7 @@ dump_config_in_collider = False
 # ==================================================================================================
 # Scan tune with step of 0.001 (need to round to correct for numpy numerical instabilities)
 array_qx = np.round(np.arange(62.305, 62.330, 0.001), decimals=4)
-array_I = np.linspace(-600, 600, 100, endpoint=True)
+array_I = np.linspace(-600, 600, 13, endpoint=True)
 
 # In case one is doing a tune-tune scan, to decrease the size of the scan, we can ignore the
 # working points too close to resonance. Otherwise just delete this variable in the loop at the end
