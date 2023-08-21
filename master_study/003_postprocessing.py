546951bc9c4043c1574d079175e14fe553563b59
Colas Droin <colasdroin@gmail.com>
2023-08-21 11:57:00 +0200

    Analysis octupolar scan

--- master_study/003_postprocessing.py
+++ master_study/003_postprocessing.py
@@ -16,7 +16,7 @@ print("Analysis of output simulation files started")
 start = time.time()
 
 # Load Data
-study_name = "example_HL_tunescan"
+study_name = "opt_collapse_1100_1500_oct_scan"
 fix = "/scans/" + study_name
 root = tree_maker.tree_from_json(fix[1:] + "/tree_maker_" + study_name + ".json")
 # Add suffix to the root node path to handle scans that are not in the root directory
@@ -75,7 +75,12 @@ for node in root.generation(1):
         df_sim["num_particles_per_bunch"] = dic_child_collider["config_beambeam"][
             "num_particles_per_bunch"
         ]
-
+        df_sim["i_oct_b1"] = dic_child_collider["config_knobs_and_tuning"]["knob_settings"][
+            "i_oct_b1"
+        ]
+        df_sim["i_oct_b2"] = dic_child_collider["config_knobs_and_tuning"]["knob_settings"][
+            "i_oct_b2"
+        ]
         # Merge with particle data
         df_sim_with_particle = pd.merge(df_sim, particle, on=["particle_id"])
         l_df_to_merge.append(df_sim_with_particle)
@@ -95,7 +100,7 @@ if df_lost_particles.empty:
     print("No unstable particles found, the output dataframe will be empty.")
 
 # Group by working point (Update this with the knobs you want to group by !)
-group_by_parameters = ["qx", "qy"]
+group_by_parameters = ["qx", "qy", "i_oct_b1", "i_oct_b2"]
 # We always want to keep beam in the final result
 group_by_parameters = ["beam"] + group_by_parameters
 l_parameters_to_keep = [
@@ -105,6 +110,8 @@ l_parameters_to_keep = [
     "i_bunch_b1",
     "i_bunch_b2",
     "num_particles_per_bunch",
+    "i_oct_b1",
+    "i_oct_b2",
 ]
 
 # Min is computed in the groupby function, but values should be identical
