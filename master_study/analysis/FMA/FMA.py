7984f84203a6f4da572b20c7e6f01cebe2dfbe86
Colas Droin <colasdroin@gmail.com>
2023-11-27 09:56:51 +0100

    Update FMA

--- master_study/analysis/FMA/FMA.py
+++ master_study/analysis/FMA/FMA.py
@@ -49,11 +49,11 @@ PARQUET_PATH = f"../../scans/{STUDY_NAME}/da.parquet"
 CONF_MAD_PATH = f"../../scans/{STUDY_NAME}/{COLLIDER}/config.yaml"
 CONF_COLLIDER_PATH = f"../../scans/{STUDY_NAME}/{COLLIDER}/xtrack_0000/config.yaml"
 
-# Define study
-collider = xt.Multiline.from_json(f"../../scans/{STUDY_NAME}/{COLLIDER}/xtrack_0000/collider.json")
+# # Define study
+# collider = xt.Multiline.from_json(f"../../scans/{STUDY_NAME}/{COLLIDER}/xtrack_0000/collider.json")
 
-# %%
-collider.build_trackers()
+# # %%
+# collider.build_trackers()
 
 # %%
 # Load configuration files
@@ -67,98 +67,98 @@ conf_collider = analysis_functions.load_config(CONF_COLLIDER_PATH)["config_colli
 range_octupoles = np.linspace(-45.0, 45, 19, endpoint=True)
 phase_change = 1.0
 
-collider.vars["phase_change.b1"] = phase_change
-collider.vars["phase_change.b2"] = phase_change
+# collider.vars["phase_change.b1"] = phase_change
+# collider.vars["phase_change.b2"] = phase_change
 l_names = []
 for idx, octupoles in enumerate(range_octupoles):
-    collider.vars["i_oct_b1"] = octupoles
-    collider.vars["i_oct_b2"] = octupoles
-    try:
-        fma = collider.lhcb1.get_fma(
-            nemitt_x=2.3e-6,
-            nemitt_y=2.3e-6,
-            n_r=25,
-            n_theta=25,
-            freeze_longitudinal=True,
-            delta0=0.0,
-        )
-    except Exception as e:
-        print(e)
-        continue
+    #     collider.vars["i_oct_b1"] = octupoles
+    #     collider.vars["i_oct_b2"] = octupoles
+    #     try:
+    #         fma = collider.lhcb1.get_fma(
+    #             nemitt_x=2.3e-6,
+    #             nemitt_y=2.3e-6,
+    #             n_r=35,
+    #             n_theta=35,
+    #             freeze_longitudinal=True,
+    #             delta0=0.0,
+    #         )
+    #     except Exception as e:
+    #         print(e)
+    #         continue
 
-    plt.figure(figsize=(5, 5))
-    workingDiagram(order=10, alpha=0.1)
-    fma.plot_fma(s=0.1)
-    plt.grid(True)
-    plt.xlim(0.2, 0.35)
-    plt.ylim(0.2, 0.35)
-    plt.title(
-        analysis_functions.get_title_from_conf(
-            conf_mad,
-            conf_collider,
-            betx=11,
-            bety=11,
-            display_intensity=True,
-            phase_knob=phase_change,
-            octupoles=octupoles,
-            emittance=2.3e-6,
-            LHC_version="Injection HL (from run III optics)",
-        )
-    )
+    #     plt.figure(figsize=(5, 5))
+    #     workingDiagram(order=10, alpha=0.1)
+    #     fma.plot_fma(s=0.1)
+    #     plt.grid(True)
+    #     plt.xlim(0.2, 0.35)
+    #     plt.ylim(0.2, 0.35)
+    #     plt.title(
+    #         analysis_functions.get_title_from_conf(
+    #             conf_mad,
+    #             conf_collider,
+    #             betx=11,
+    #             bety=11,
+    #             display_intensity=True,
+    #             phase_knob=phase_change,
+    #             octupoles=octupoles,
+    #             emittance=2.3e-6,
+    #             LHC_version="Injection HL (from run III optics)",
+    #         )
+    #     )
     name = "plots/" + "{:02d}".format(idx) + f"_fma_phase_{phase_change}.png"
-    plt.savefig(name, dpi=300)
+    #     plt.savefig(name, dpi=300)
     l_names.append(name)
-    plt.close()
+#     plt.close()
 
 # Generating the gif with all the frames:
-# frames = np.stack([iio.imread(name) for name in l_names], axis=0)
-# iio.imwrite(f"plots/fma_phase_{phase_change}.gif", frames)
+frames = np.stack([iio.imread(name) for name in l_names], axis=0)
+iio.imwrite(f"plots/fma_phase_{phase_change}.gif", frames, fps=2)
 
 
 phase_change = 0.0
-collider.vars["phase_change.b1"] = phase_change
-collider.vars["phase_change.b2"] = phase_change
+# collider.vars["phase_change.b1"] = phase_change
+# collider.vars["phase_change.b2"] = phase_change
 l_names = []
 for idx, octupoles in enumerate(range_octupoles):
-    collider.vars["i_oct_b1"] = octupoles
-    collider.vars["i_oct_b2"] = octupoles
-    try:
-        fma = collider.lhcb1.get_fma(
-            nemitt_x=2.3e-6,
-            nemitt_y=2.3e-6,
-            n_r=25,
-            n_theta=25,
-            freeze_longitudinal=True,
-            delta0=0.0,
-        )
-    except Exception as e:
-        print(e)
-        continue
+    #     collider.vars["i_oct_b1"] = octupoles
+    #     collider.vars["i_oct_b2"] = octupoles
+    #     try:
+    #         fma = collider.lhcb1.get_fma(
+    #             nemitt_x=2.3e-6,
+    #             nemitt_y=2.3e-6,
+    #             n_r=35,
+    #             n_theta=35,
+    #             freeze_longitudinal=True,
+    #             delta0=0.0,
+    #         )
+    #     except Exception as e:
+    #         print(e)
+    #         continue
 
-    plt.figure(figsize=(5, 5))
-    workingDiagram(order=10, alpha=0.1)
-    fma.plot_fma(s=0.1)
-    plt.grid(True)
-    plt.xlim(0.2, 0.35)
-    plt.ylim(0.2, 0.35)
-    plt.title(
-        analysis_functions.get_title_from_conf(
-            conf_mad,
-            conf_collider,
-            betx=11,
-            bety=11,
-            display_intensity=True,
-            phase_knob=phase_change,
-            octupoles=octupoles,
-            emittance=2.3e-6,
-            LHC_version="Injection HL (from run III optics)",
-        )
-    )
+    #     plt.figure(figsize=(5, 5))
+    #     workingDiagram(order=10, alpha=0.1)
+    #     fma.plot_fma(s=0.1)
+    #     plt.grid(True)
+    #     plt.xlim(0.2, 0.35)
+    #     plt.ylim(0.2, 0.35)
+    #     plt.title(
+    #         analysis_functions.get_title_from_conf(
+    #             conf_mad,
+    #             conf_collider,
+    #             betx=11,
+    #             bety=11,
+    #             display_intensity=True,
+    #             phase_knob=phase_change,
+    #             octupoles=octupoles,
+    #             emittance=2.3e-6,
+    #             LHC_version="Injection HL (from run III optics)",
+    #         )
+    #     )
     name = "plots/" + "{:02d}".format(idx) + f"_fma_phase_{phase_change}.png"
-    plt.savefig(name, dpi=300)
+    #     plt.savefig(name, dpi=300)
     l_names.append(name)
-    plt.close()
+#     plt.close()
 
 # Generating the gif with all the frames:
-# frames = np.stack([iio.imread(name) for name in l_names], axis=0)
-# iio.imwrite(f"plots/fma_phase_{phase_change}.gif", frames)
+frames = np.stack([iio.imread(name) for name in l_names], axis=0)
+iio.imwrite(f"plots/fma_phase_{phase_change}.gif", frames, fps=2)
