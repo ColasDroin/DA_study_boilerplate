6167a83474552328da9b38520dec35ae6a2e9aa5
Colas <colasdroin@gmail.com>
2023-12-13 10:37:41 +0100

    Removed the old study

--- master_study/scans/dynamic_collapse_old/base_collider/xtrack_0000_precompute_resetonbbscale/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-source /afs/cern.ch/work/c/cdroin/private/example_DA_study/master_study/../activate_miniforge.sh
-cd /afs/cern.ch/work/c/cdroin/private/example_DA_study/master_study/scans/dynamic_collapse/base_collider/xtrack_0000_precompute_resetonbbscale
-python 2_configure_and_track.py > output_python.txt 2> error_python.txt
-rm -rf final_* modules optics_repository optics_toolkit tools tracking_tools temp mad_collider.log __pycache__ twiss* errors fc* optics_orbit_at*
