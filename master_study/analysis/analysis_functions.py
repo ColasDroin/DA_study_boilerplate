f6d1de67926a1cf0460d57ffc27a90b65e6303f1
Colas Droin <colasdroin@gmail.com>
2024-03-06 17:20:15 +0100

    More digits for bunch charge

--- master_study/analysis/analysis_functions.py
+++ master_study/analysis/analysis_functions.py
@@ -56,7 +56,7 @@ def add_QR_code(fig, link):
 
 # Function to convert floats to scientific latex format
 def latex_float(f):
-    float_str = "{0:.2g}".format(f)
+    float_str = "{0:.4g}".format(f)
     if "e" in float_str:
         base, exponent = float_str.split("e")
         return r"${0} \times 10^{{{1}}}$".format(base, int(exponent))
