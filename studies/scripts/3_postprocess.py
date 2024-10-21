1a02f1fe7916ff92bf9ecdb68ab0be9330ea072d
Colas Droin <colasdroin@gmail.com>
2024-10-21 09:19:28 +0200

    Got results analysis new tune scan

--- studies/scripts/3_postprocess.py
+++ studies/scripts/3_postprocess.py
@@ -20,9 +20,7 @@ import yaml
 #     "tunescan_50cm_2024_agressive_3",
 #     "tunescan_50cm_control_sofia_IPAC",
 # ]
-l_study_name = [
-    "new_tunescan_50cm_2024_agressive",
-]
+l_study_name = ["new_tunescan_50cm_2024_agressive", "new_tunescan_50cm_2024_agressive_alt"]
 
 # study_name = "collider_50cm_2024"
 for study_name in l_study_name:
